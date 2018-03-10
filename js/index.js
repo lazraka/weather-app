36117689a3740fe278c32129e421bac1d90a04f4
Amina Lazrak <amina.lazrak@gmail.com>
2018-03-10 17:16:18 -0500

    Add season statement

--- js/index.js
+++ js/index.js
@@ -4,23 +4,27 @@
 var num;
 document.getElementById('submit-button').onclick = function(){
 	num=parseInt(document.getElementById('fieldname').value)
-	document.getElementById('celsius').innerHTML = num;
+	document.getElementById('celsius').innerHTML = "The temperature in Celsius: "+num;
 	num = (num * 1.8) + 32;
-	document.getElementById('fahrenheit').innerHTML = num;
+	document.getElementById('fahrenheit').innerHTML = "The temperature in Fahrenheit: "+num;
 	determineSeason(num);
 }
 
 function determineSeason(temp){
 	if (temp <= 32){
 		document.body.style.backgroundColor = "grey";
+		document.getElementById('season').innerHTML = "It is now Winter!";
 	}
 	else if (temp>32 && temp<=50){
 		document.body.style.backgroundColor = "blue";
+		document.getElementById('season').innerHTML = "It is now Fall!";
 		}
 	else if (temp>50 && temp<=80){
 		document.body.style.backgroundColor = "orange";
+		document.getElementById('season').innerHTML = "It is now Spring!";
 		}
 	else if (temp>80){
 		document.body.style.backgroundColor = "yellow";
+		document.getElementById('season').innerHTML = "It is now Summer!";
 		}
 }
